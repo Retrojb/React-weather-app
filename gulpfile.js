0a2f8713bc028f79dd513fa473fa6a989546d5a2
Retrojb <baltes.26@osu.edu>
2018-08-12 16:45:01 -0400

    gulp.js is built

--- gulpfile.js
+++ gulpfile.js
@@ -29,3 +29,68 @@ var notify = function (error) {
 
     notifier.notify({title: title, message: message});
 }
+
+// bundle settings
+
+let bundler = watchify(browserify({
+    entries: ['./src/app.jsx'],
+    transform:[reactify],
+    extensions: ['.jsx'],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+}));
+
+//bundle tasks
+
+function bundle(){
+    return bundler.bundle()
+    .on('error', notify)
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('./'))
+}
+
+bundler.on('update', bundle);
+
+//create bundle
+
+gulp.task('build', function(){
+    bundle()
+});
+
+//Compile the SASS files from main.scss
+
+gulp.task('sass', function(){
+    gulp.src('./sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('style.css'))
+.pipe(gulp.dest('./'));
+});
+
+//live reload
+
+gulp.task('serve', function(done){
+    gulp.sec('')
+    .pipe(server({
+        livereload:{
+            enable: true,
+            filter: function(filePath, cb){
+                if(/main.js/.test(filePath)){
+                cb(true)
+            }
+        }
+    },
+    opne:true
+}));
+});
+
+//checks for changes in sass files
+
+gulp.task('watch', function (){
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+//run test in specified order
+
+gulp.task('default', ['build', 'serve', 'sass', 'watch']);
