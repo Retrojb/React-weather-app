6ed9126a4ca763342c4c623db75922ed92a462c1
Retrojb <baltes.26@osu.edu>
2018-08-12 18:14:40 -0400

    api.jsx built

---
+++ src/utils/api.jsx
@@ -0,0 +1,16 @@
+let Fetch = require('whatwg-fetch');
+let rootUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+let apiUrl = '&appid=2de143494c0b295cca9337e1e96b00e0';
+
+module.exports = {
+    get: function(place){
+        return fetch(rootUrl + place +apiUrl, {
+            headers: {
+
+            }
+        })
+        .then(function(response){
+            return response.json();
+        });
+    }
+};
